0408d9b491452b50aac5059fbd51f8e981e1b3e5
Dan Brimhall <dpbrimhall@hotmail.com>
2018-10-16 23:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
+<<<<<<< HEAD
  var nested = document.getElementsByClassName('target') 
  return nested[0]
 }
 
 
+=======
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+>>>>>>> 5967b53ee35f4c77332f6c8a644d904670e07fff
 function increaseRankBy(n) {
   const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < list.length; i++) {
